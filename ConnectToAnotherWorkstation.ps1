db57fb59742a173693519fb92952fc23459a29cd
jakers821 <jakers821@users.noreply.github.com>
2017-03-24 12:25:59 -0700

    First Iteration
    This is just a simple program for connecting to other computers on the domain. May add more functionality to it.

---
+++ ConnectToAnotherWorkstation.ps1
@@ -0,0 +1,9 @@
+﻿set-executionpolicy unrestricted
+
+$connectionComputer = Read-Host -Prompt "Hello, what computer would you like to connect to?"
+
+$cred = get-credential 
+
+Enter-PSSession $connectionComputer -Credential $cred
+
+
